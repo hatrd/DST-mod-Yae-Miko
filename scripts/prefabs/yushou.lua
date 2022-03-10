5eae9bfcc659e0bdee249082bf5f80810e435ee9
hatrd <ettaii@qq.com>
2022-03-10 21:30:06 +0800

    御守小幅度恢复sanity

--- scripts/prefabs/yushou.lua
+++ scripts/prefabs/yushou.lua
@@ -1,6 +1,5 @@
 local assets={
 Asset("ANIM", "anim/yushou.zip"),
-Asset("ANIM", "anim/swap_yushou.zip"),
 Asset("ATLAS", "images/inventoryimages/yushou.xml"),
 Asset("IMAGE", "images/inventoryimages/yushou.tex"),
 }
@@ -9,18 +8,30 @@ local prefabs = {"yushou"}
 
 local function OnEquip(inst, owner)
 
-  owner.AnimState:OverrideSymbol("swap_object", "swap_yushou", "swap_yushou")
+  owner.AnimState:OverrideSymbol("swap_hat", "yushou", "swap_yushou")
 
-  owner.AnimState:Show("ARM_carry")
 
-  owner.AnimState:Hide("ARM_normal")
+  owner.AnimState:Show("HAT")
+
+--平时恢复san
+  inst.components.equippable.dapperness = TUNING.DAPPERNESS_SMALL
+--做成佩戴御守战斗时恢复san值
+  -- if owner.components.sanity ~= nil then
+  --   owner.components.sanity:DoDelta(100, true) -- using overtime so it doesnt make the sanity sfx every time you attack
+  -- end
+	
 end
 
 local function OnUnequip(inst, owner)
 
-  owner.AnimState:Hide("ARM_carry")
+	owner.AnimState:ClearOverrideSymbol("swap_yushou")
+	
+	-- Show/Hide some of the layers of the character while unequipping the hat.
 
-  owner.AnimState:Show("ARM_normal")
+  owner.AnimState:Hide("HAT")
+
+	
+
 end
 
 local function fn()
@@ -29,7 +40,8 @@ local function fn()
   local anim = inst.entity:AddAnimState()
   local sound = inst.entity:AddSoundEmitter()
   MakeInventoryPhysics(inst)
-  
+
+
   STRINGS.NAMES.YUSHOU = "御守"
   STRINGS.RECIPE_DESC.YUSHOU = "想我的时候也可以拿出来看看哦？"
   STRINGS.CHARACTERS.GENERIC.DESCRIBE.YUSHOU = "这是才智与美貌兼具的八重神子大人赠与我的！"
@@ -37,6 +49,15 @@ local function fn()
   anim:SetBank("yushou")
   anim:SetBuild("yushou")
   anim:PlayAnimation("idle")
+
+
+  
+inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
   inst:AddComponent("inspectable")
   inst:AddComponent("tradable")
   inst:AddComponent("inventoryitem")
@@ -44,9 +65,11 @@ local function fn()
   inst.components.inventoryitem.atlasname = "images/inventoryimages/yushou.xml"
   
 
-  -- inst:AddComponent("equippable")
-  -- inst.components.equippable:SetOnEquip( OnEquip )
-  -- inst.components.equippable:SetOnUnequip( OnUnequip )
+  inst:AddComponent("equippable")
+  inst.components.equippable.equipslot = EQUIPSLOTS.HEAD
+  inst.components.equippable:SetOnEquip(OnEquip)
+  inst.components.equippable:SetOnUnequip(OnUnequip)
   return inst
 end
+
 return  Prefab("common/inventory/yushou", fn, assets, prefabs)
