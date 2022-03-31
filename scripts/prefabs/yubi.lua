df00fb5c70842960cc30c90c5bde6b8b7434e5d1
hatrd <ettaii@qq.com>
2022-03-31 20:14:39 +0800

    御币地面，基本攻击实现

--- scripts/prefabs/yubi.lua
+++ scripts/prefabs/yubi.lua
@@ -1,5 +1,6 @@
 local assets={
 Asset("ANIM", "anim/yubi.zip"),
+Asset("ANIM", "anim/swap_yubi.zip"),
 Asset("ATLAS", "images/inventoryimages/yubi.xml"),
 Asset("IMAGE", "images/inventoryimages/yubi.tex"),
 }
@@ -7,15 +8,16 @@ Asset("IMAGE", "images/inventoryimages/yubi.tex"),
 local prefabs = {"yubi"}
 
 local function OnEquip(inst, owner)
-
-
-
+    -- owner.AnimState:OverrideSymbol("swap_hat", "yushou", "swap_yushou")
+    -- owner.AnimState:OverrideSymbol("swap_object", "custom_handitem", "swap_object")
+    -- owner.AnimState:OverrideSymbol("swap_object", "swap_yubi", "swap_object")
+    owner.AnimState:Show("ARM_carry")
+    owner.AnimState:Hide("ARM_normal")
 end
 
 local function OnUnequip(inst, owner)
-
-
-
+    owner.AnimState:Hide("ARM_carry") 
+    owner.AnimState:Show("ARM_normal")
 end
 
 local function fn()
@@ -29,13 +31,17 @@ local function fn()
 
 
   STRINGS.NAMES.YUBI = "御币"
-  STRINGS.RECIPE_DESC.YUBI = "八重神子的武器。"
-  STRINGS.CHARACTERS.GENERIC.DESCRIBE.YUBI = "似乎可以祓除邪恶。"
+  STRINGS.RECIPE_DESC.YUBI = "似乎可以祓除邪恶。"
+  STRINGS.CHARACTERS.GENERIC.DESCRIBE.YUBI = "八重神子的武器。"
 
   anim:SetBank("yubi")
   anim:SetBuild("yubi")
-  anim:PlayAnimation("idle")
+  anim:PlayAnimation("idle",true)
 
+  inst:AddComponent("weapon")
+  inst.components.weapon:SetDamage(12)
+  inst.components.weapon:SetRange(8, 12)
+  -- inst.components.weapon:SetOnAttack(onattack_yubi)
 
   
   inst.entity:SetPristine()
@@ -52,6 +58,7 @@ local function fn()
   
 
   inst:AddComponent("equippable")
+  inst.components.equippable.restrictedtag = "yaemiko"
   inst.components.equippable:SetOnEquip(OnEquip)
   inst.components.equippable:SetOnUnequip(OnUnequip)
   return inst
