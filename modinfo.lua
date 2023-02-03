3fece75c686bec50c2c6536c0d0aa6a433755316
hatrd <ettaii@qq.com>
2023-02-03 13:32:46 +0800

    1.0.9 技能更新。

--- modinfo.lua
+++ modinfo.lua
@@ -5,7 +5,7 @@ Press "Z" to use ELemental Skill.
 Press "X" to use Elemental Burst.
 ]]
 author = "NaNaN"
-version = "1.0.7"
+version = "1.0.9"
 forumthread = ""
 api_version = 10
 dst_compatible = true
