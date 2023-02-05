900315413db8bbcb462fb0f2a88dac6d7c0a1d0c
hatrd <ettaii@qq.com>
2023-02-05 17:14:17 +0800

    删除随版本更新而用不上的图片

--- modmain.lua
+++ modmain.lua
@@ -10,12 +10,6 @@ Assets = {
     Asset( "IMAGE", "images/saveslot_portraits/yaemiko.tex" ),
     Asset( "ATLAS", "images/saveslot_portraits/yaemiko.xml" ),
 
-    -- Asset( "IMAGE", "images/selectscreen_portraits/yaemiko.tex" ),
-    -- Asset( "ATLAS", "images/selectscreen_portraits/yaemiko.xml" ),
-	
-    -- Asset( "IMAGE", "images/selectscreen_portraits/yaemiko_silho.tex" ),
-    -- Asset( "ATLAS", "images/selectscreen_portraits/yaemiko_silho.xml" ),
-
     Asset( "IMAGE", "bigportraits/yaemiko.tex" ),
     Asset( "ATLAS", "bigportraits/yaemiko.xml" ),
 	
@@ -36,10 +30,6 @@ Assets = {
 	
 	Asset( "IMAGE", "images/names_gold_yaemiko.tex" ),
     Asset( "ATLAS", "images/names_gold_yaemiko.xml" ),
-
-    
-	-- Asset( "IMAGE", "images/hud/paimon.tex" ),
-	-- Asset( "ATLAS", "images/hud/paimon.xml" ),
 
 	Asset( "IMAGE", "images/skills/yaemiko_skill_0.tex" ),
 	Asset( "ATLAS", "images/skills/yaemiko_skill_0.xml" ),
