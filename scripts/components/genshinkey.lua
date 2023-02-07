b3b1b7d07173884c48878a0c9b70e37dd621c30c
hatrd <ettaii@qq.com>
2023-02-07 22:00:23 +0800

    多人游戏，两个神子有bug。主机神子按Z，客机也会使用元素战技。尝试更新genshinkey.lua

--- scripts/components/genshinkey.lua
+++ scripts/components/genshinkey.lua
@@ -2,73 +2,57 @@ local GenshinKey = Class(function(self, inst)
 	self.inst = inst
 end)
 
-function GenshinKey:Press(Key, Action, Target, arg)
+function GenshinKey:Press(Key, Action, target)
 	TheInput:AddKeyDownHandler(Key, function()
-		if TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
-			if TheWorld.ismastersim then
-				BufferedAction(self.inst, Target, ACTIONS[string.upper(Action)]):Do()
-			else
-				SendModRPCToServer(MOD_RPC[self.inst.prefab][Action], arg)
-			end
+		if self.inst == ThePlayer and TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
+			SendModRPCToServer(MOD_RPC[self.inst.prefab][Action], target)
 		end
 	end)
 end
 
-function GenshinKey:Hold(Key, Start, Stop, Target, arg)
+function GenshinKey:Charge(Key, Start, Stop, target)
 	TheInput:AddKeyDownHandler(Key, function()
-		if TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
-			if not self.inst:HasTag("act_holding") then
-				if TheWorld.ismastersim then
-					BufferedAction(self.inst, Target, ACTIONS[string.upper(Start)]):Do()
-				else
-					SendModRPCToServer(MOD_RPC[self.inst.prefab][Start], arg)
-				end
-				self.inst:AddTag("act_holding")
+		if self.inst == ThePlayer and TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
+			if not self.ishold then
+				SendModRPCToServer(MOD_RPC[self.inst.prefab][Start], target)
+				self.ishold = true
+				self.inst:AddTag("charging")
 			end
 		end
 	end)
 	TheInput:AddKeyUpHandler(Key, function()
-		if TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
-			if self.inst:HasTag("act_holding") then
-				if TheWorld.ismastersim then
-					BufferedAction(self.inst, Target, ACTIONS[string.upper(Stop)]):Do()
-				else
-					SendModRPCToServer(MOD_RPC[self.inst.prefab][Stop], arg)
-				end
-				self.inst:RemoveTag("act_holding")
+		if self.inst == ThePlayer and TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
+			if self.ishold then
+				SendModRPCToServer(MOD_RPC[self.inst.prefab][Stop], target)
+				self.ishold = false
+				self.inst:RemoveTag("charging")
 			end
 		end
 	end)
 end
 
-function GenshinKey:Event(Key, Press, Hold, Target, arg)
+function GenshinKey:Event(Key, Press, Hold, target)
 	TheInput:AddKeyDownHandler(Key, function()
-		if TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
-			if not self.inst:HasTag("hold_event") then
-				self.inst:AddTag("act_event")
-				self.inst:DoTaskInTime(0.3, function()
-					self.inst:RemoveTag("act_event")
-					self.inst:AddTag("hold_event")
+		if self.inst == ThePlayer and TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
+			if not self.holding then
+				self.pending = true
+				self.holding = true
+				self.inst:DoTaskInTime(0.6, function()
+					if self.holding then
+						SendModRPCToServer(MOD_RPC[self.inst.prefab][Hold], target)
+					end
 				end)
 			end
 		end
 	end)
 	TheInput:AddKeyUpHandler(Key, function()
-		if TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
-			if self.inst:HasTag("act_event") then
-				if TheWorld.ismastersim then
-					BufferedAction(self.inst, Target, ACTIONS[string.upper(Press)]):Do()
-				else
-					SendModRPCToServer(MOD_RPC[self.inst.prefab][Press], arg)
+		if self.inst == ThePlayer and TheFrontEnd:GetActiveScreen() and TheFrontEnd:GetActiveScreen().name == "HUD" then
+			if self.pending then
+				SendModRPCToServer(MOD_RPC[self.inst.prefab][Press], target)
+				self.pending = false
+				if self.holding then
+					self.holding = false
 				end
-				self.inst:DoTaskInTime(0.3, function() self.inst:RemoveTag("hold_event") end)
-			else
-				if TheWorld.ismastersim then
-					BufferedAction(self.inst, Target, ACTIONS[string.upper(Hold)]):Do()
-				else
-					SendModRPCToServer(MOD_RPC[self.inst.prefab][Hold], arg)
-				end
-				self.inst:RemoveTag("hold_event")
 			end
 		end
 	end)
